8a526986e9c4e7094beb174f4e616e55825479d4
Ashwini shalke <ashwinishalke050@gmail.com>
2019-12-13 13:34:03 +0530

    Intial stage of Facebook popup animations

---
+++ FacebookPopUpAnimations/FacebookPopUpAnimations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FacebookPopUpAnimations
+//
+//  Created by Ashwini shalke on 13/12/19.
+//  Copyright © 2019 Ashwini Shalke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ FacebookPopUpAnimations/FacebookPopUpAnimationsTests/FacebookPopUpAnimationsTests.swift
@@ -0,0 +1,34 @@
+//
+//  FacebookPopUpAnimationsTests.swift
+//  FacebookPopUpAnimationsTests
+//
+//  Created by Ashwini shalke on 13/12/19.
+//  Copyright © 2019 Ashwini Shalke. All rights reserved.
+//
+
+import XCTest
+@testable import FacebookPopUpAnimations
+
+class FacebookPopUpAnimationsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
